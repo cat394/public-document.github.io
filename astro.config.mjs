68d1c555963bc800175b4cdbd6ceeaec6275bc26
Ryo <91322302+cat394@users.noreply.github.com>
2023-11-09 11:33:19 +0900

    feat: publish github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://cat394.github.io',
 	integrations: [
 		starlight({
 			title: "Ryo's Doc",
